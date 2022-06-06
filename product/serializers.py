fe58556c16c9dc7546e28d9a61dfc16450355940
Vlad <44709282+aposDupos@users.noreply.github.com>
2022-06-06 12:02:54 +0300

    initial

---
+++ product/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework.serializers import ModelSerializer, IntegerField
+from .models import Order, Product, Provider, User
+
+
+class ProviderSerializer(ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = '__all__'
+
+
+class ProductSerializer(ModelSerializer):
+    provider_data = ProviderSerializer(source='provider')
+
+    class Meta:
+        model = Product
+        exclude = ['provider', ]
+
+
+class OrderSerializer(ModelSerializer):
+    sum = IntegerField(source='get_total_price')
+    products = ProductSerializer(source='product', many=True)
+
+    class Meta:
+        model = Order
+        exclude = ['product', ]
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
